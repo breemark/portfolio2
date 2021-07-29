af92572644feb8f72125de9442d0ed531d92a193
torres <britomaximiliano83@gmail.com>
2021-07-29 07:11:21 +0700

    frontpage model

--- frontpage/models.py
+++ frontpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class FrontPage(models.Model):
+    """Front page of your Portfolio. Only the latest version will be loaded"""
+    bio = models.TextField(null=True, blank=True)
+    cv_resume = models.FileField(blank=True, null=True, upload_to='resume/')
+    email = models.CharField(max_length=120, null=True, blank=True)
+    phone = models.CharField(max_length=120, null=True, blank=True)
+    skype = models.CharField(max_length=120, null=True, blank=True)
+    wechat = models.CharField(max_length=120, null=True, blank=True)
+    facebook = models.CharField(max_length=120, null=True, blank=True)
+    instagram = models.CharField(max_length=120, null=True, blank=True)
