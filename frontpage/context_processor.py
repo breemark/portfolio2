23ef51b579020ea921750289ddd6210c5ab08474
torres <britomaximiliano83@gmail.com>
2021-08-12 23:21:47 -0500

    show posts if not empty

--- frontpage/context_processor.py
+++ frontpage/context_processor.py
@@ -1,8 +1,20 @@
 from django.conf import settings
 from .models import Language
+from articles.models import Article
+from projects.models import Project
 
 
 def languages_processor(request):
     current_language = request.LANGUAGE_CODE
     languages = Language.objects.all()
-    return {'current_language': current_language, 'languages': languages}
+    articles = Article.objects.all()
+    projects = Project.objects.all()
+
+    context = {
+        'current_language': current_language, 
+        'languages': languages,
+        'articles': articles,
+        'projects': projects,
+    }
+
+    return context
